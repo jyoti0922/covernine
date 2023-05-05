64d706304551a3718064b23d3530307b94315b20
jangra-rahul <rjradialcode@gmail.com>
2023-05-05 16:15:20 +0530

    section add and color

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import About from "./components/About";
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <About />
     </>
   );
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import about_image from "../assets/images/png/about_bg.png";
 
 const About = () => {
   return (
     <>
-      <section className=" min-vh-100 "></section>
+      <section className=" min-vh-100 ">
+        <img className="w-100" src={about_image} alt="about_image" />
+      </section>
     </>
   );
 };
